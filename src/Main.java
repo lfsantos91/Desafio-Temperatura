26eb68c6162ae1307fb8eccf8a6ab3ebaa4902f7
lfsantos91 <42644049+lfsantos91@users.noreply.github.com>
2023-04-10 09:18:17 -0300

    Desafio Temperatura
    Desafio praticado em aula!

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        double temperaturaEmCelsius = 32.4;
+        double temperaturaEmFahrenheit = (temperaturaEmCelsius * 1.8) +32;
+
+        String mensagem = String.format("A temperatura de %f em Celsius é equivalente a %f Fahrenheit", temperaturaEmCelsius, temperaturaEmFahrenheit);
+
+        System.out.println(mensagem);
+
+        int temperaturaEmFahrenheitInteira = (int) temperaturaEmFahrenheit;
+        System.out.println("A temperatura em Fahrenheit inteira é: " + temperaturaEmFahrenheitInteira);
+    }
+}
